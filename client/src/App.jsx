6288c1d3864fd5f25878beb390626f8ddc3d4a62
Jabed Ali Mollah <jabedalimollah7@gmail.com>
2024-08-26 12:08:55 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-dvh flex justify-center items-center bg-green-100">
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
